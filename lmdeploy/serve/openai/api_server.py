90c8f09b0e00b590e56175081a8d4ba1e1efb567
xinmeng.wang <xinmeng.wang@nio.com>
2023-12-05 12:58:16 +0800

    feat: support metric

--- lmdeploy/serve/openai/api_server.py
+++ lmdeploy/serve/openai/api_server.py
@@ -22,7 +22,7 @@ from lmdeploy.serve.openai.protocol import (  # noqa: E501
     GenerateRequest, GenerateResponse, ModelCard, ModelList, ModelPermission,
     UsageInfo)
 
-if os.environ['TM_LOG_LEVEL'] is None or os.environ['TM_LOG_LEVEL'] == '':
+if not 'TM_LOG_LEVEL' in os.environ or os.environ['TM_LOG_LEVEL'] == '':
     os.environ['TM_LOG_LEVEL'] = 'INFO'
 
 class VariableInterface:
@@ -43,7 +43,15 @@ instrumentator = Instrumentator(
 
 @app.on_event("startup")
 async def _startup():
+    # set ENABLE_METRICS to True to enable metrics
     instrumentator.expose(app)
+
+
+@app.middleware("http")
+# add body logger
+async def add_logger(request: Request, call_next):
+    pass
+
 
 
 def get_model_list():
